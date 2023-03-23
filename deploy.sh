4e334d7d0e6598e8d5de3480c6d043e50a39ae5e
panduola666 <panduola666@gmail.com>
2023-03-24 01:25:33 +0800

    config 加上 /* global process */

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@
     git init
     git checkout -B main
     git add -A
-    git commit -m '訂單畫面修復'
+    git commit -m 'config 加上 /* global process */'
 
     # if you are deploying to https://panduola666.github.io
     # git push -f git@github.com:panduola666/panduola666.github.io.git main
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+/* global process */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base:
